119ff3264e1fae08e633d0085c99a20606580aa0
Armin Ronacher <armin.ronacher@active-4.com>
2009-10-10 17:00:05 +0200

    New release

--- phpserialize.py
+++ phpserialize.py
@@ -84,8 +84,150 @@ r"""
     This feature however is not supported in PHP.  PHP will only unserialize
     the first object.
 
-    CHANGELOG
+    Array Serialization
+    ===================
+
+    Starting with 1.2 you can provide an array hook to the unserialization
+    functions that are invoked with a list of pairs to return a real array
+    object.  By default `dict` is used as array object which however means
+    that the information about the order is list for associative arrays.
+
+    For example you can pass the ordered dictionary to the unserilization
+    functions:
+
+    >>> from collections import OrderedDict
+    >>> loads('a:2:{s:3:"foo";i:1;s:3:"bar";i:2;}',
+    ...       array_hook=OrderedDict)
+    collections.OrderedDict([('foo', 1), ('bar', 2)])
+
+    Object Serialization
+    ====================
+
+    PHP supports serialization of objects.  Starting with 1.2 of phpserialize
+    it is possible to both serialize and unserialize objects.  Because class
+    names in PHP and Python usually do not map, there is a separate
+    `object_hook` parameter that is responsible for creating these classes.
+
+    For a simple test example the `phpserialize.phpobject` class can be used:
+
+    >>> data = 'O:7:"WP_User":1:{s:8:"username";s:5:"admin";}'
+    >>> user = loads(data, object_hook=phpobject)
+    >>> user.username
+    'admin'
+    >>> user.__name__
+    'WP_User'
+
+    An object hook is a function that takes the name of the class and a dict
+    with the instance data as arguments.  The instance data keys are in PHP
+    format which usually is not what you want.  To convert it into Python
+    identifiers you can use the `convert_member_dict` function.  For more
+    information about that, have a look at the next section.  Here an
+    example for a simple object hook:
+
+    >>> class User(object):
+    ...     def __init__(self, username):
+    ...         self.username = username
+    ...
+    >>> def object_hook(name, d):
+    ...     cls = {'WP_User': User}[name]
+    ...     return cls(**d)
+    ...
+    >>> user = loads(data, object_hook=phpobject)
+    >>> user.username
+    'admin'
+
+    To serialize objects you can use the `object_hook` of the dump functions
+    and return instances of `phpobject`:
+
+    >>> def object_hook(obj):
+    ...     if isinstance(obj, User):
+    ...         return phpobject('WP_User', {'username': obj.username})
+    ...     raise LookupError('unknown object')
+    ...
+    >>> dumps(user, object_hook=object_hook)
+    'O:7:"WP_User":1:{s:8:"username";s:5:"admin";}'
+
+    PHP's Object System
+    ===================
+
+    The PHP object system is derived from compiled languages such as Java
+    and C#.  Attributes can be protected from external access by setting
+    them to `protected` or `private`.  This does not only serve the purpose
+    to encapsulate internals but also to avoid name clashes.
+
+    In PHP each class in the inheritance chain can have a private variable
+    with the same name, without causing clashes.  (This is similar to the
+    Python `__var` name mangling system).
+
+    This PHP class::
+
+        class WP_UserBase {
+            protected $username;
+
+            public function __construct($username) {
+                $this->username = $username;
+            }
+        }
+
+        class WP_User extends WP_UserBase {
+            private $password;
+            public $flag;
+
+            public function __construct($username, $password) {
+                parent::__construct($username);
+                $this->password = $password;
+                $this->flag = 0;
+            }
+        }
+
+    Is serialized with a member data dict that looks like this:
+
+    >>> data = {
+    ...     ' * username':          'the username',
+    ...     ' WP_User password':    'the password',
+    ...     'flag':                 'the flag'
+    ... }
+
+    Because this access system does not exist in Python, the
+    `convert_member_dict` can convert this dict:
+
+    >>> d = convert_member_dict(data)
+    >>> d['username']
+    'the username'
+    >>> d['password']
+    'the password'
+
+    The `phpobject` class does this conversion on the fly.  What is
+    serialized is the special `__php_vars__` dict of the class:
+
+    >>> user = phpobject('WP_User', data)
+    >>> user.username
+    'the username'
+    >>> user.username = 'admin'
+    >>> user.__php_vars__[' * username']
+    'admin'
+
+    As you can see, reassigning attributes on a php object will try
+    to change a private or protected attribute with the same name.
+    Setting an unknown one will create a new public attribute:
+
+    >>> user.is_admin = True
+    >>> user.__php_vars__['is_admin']
+    True
+
+    To convert the phpobject into a dict, you can use the `_asdict`
+    method:
+
+    >>> d = user._asdict()
+    >>> d['username']
+    'admin'
+
+    Changelog
     =========
+
+    1.2
+        -   added support for object serialization
+        -   added support for array hooks.
 
     1.1
         -   added `dict_to_list` and `dict_to_tuple`
@@ -100,9 +242,65 @@ from StringIO import StringIO
 
 __author__ = 'Armin Ronacher <armin.ronacher@active-4.com>'
 __version__ = '1.1'
+__all__ = ('phpobject', 'convert_member_dict', 'dict_to_list', 'dict_to_tuple',
+           'load', 'loads', 'dump', 'dumps', 'serialize', 'unserialize')
 
 
-def dumps(data, charset='utf-8', errors='strict'):
+def _translate_member_name(name):
+    if name[:1] == ' ':
+        name = name.split(None, 2)[-1]
+    return name
+
+
+class phpobject(object):
+    """Simple representation for PHP objects.  This is used """
+    __slots__ = ('__name__', '__php_vars__')
+
+    def __init__(self, name, d=None):
+        if d is None:
+            d = {}
+        object.__setattr__(self, '__name__', name)
+        object.__setattr__(self, '__php_vars__', d)
+
+    def _asdict(self):
+        """Returns a new dictionary from the data with Python identifiers."""
+        return convert_member_dict(self.__php_vars__)
+
+    def _lookup_php_var(self, name):
+        for key, value in self.__php_vars__.iteritems():
+            if _translate_member_name(key) == name:
+                return key, value
+
+    def __getattr__(self, name):
+        rv = self._lookup_php_var(name)
+        if rv is not None:
+            return rv[1]
+        raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        rv = self._lookup_php_var(name)
+        if rv is not None:
+            name = rv[0]
+        self.__php_vars__[name] = value
+
+    def __repr__(self):
+        return '<phpobject %r>' % (self.__name__,)
+
+
+def convert_member_dict(d):
+    """Converts the names of a member dict to Python syntax.  PHP class data
+    member names are not the plain identifiers but might be prefixed by the
+    class name if private or a star if protected.  This function converts them
+    into standard Python identifiers:
+
+    >>> convert_member_dict({"username": "user1", " User password":
+    ...                      "default", " * is_active": True})
+    {'username': 'user1', 'password': 'default', 'is_active': True}
+    """
+    return dict((_translate_member_name(k), v) for k, v in d.iteritems())
+
+
+def dumps(data, charset='utf-8', errors='strict', object_hook=None):
     """Return the PHP-serialized representation of the object as a string,
     instead of writing it to a file like `dump` does.
     """
@@ -140,11 +338,19 @@ def dumps(data, charset='utf-8', errors='strict'):
                     out.append(_serialize(key, True))
                     out.append(_serialize(value, False))
                 return 'a:%i:{%s}' % (len(obj), ''.join(out))
+            if isinstance(obj, phpobject):
+                return 'O%s%s' % (
+                    _serialize(obj.__name__, True)[1:-1],
+                    _serialize(obj.__php_vars__, False)[1:]
+                )
+            if object_hook is not None:
+                return _serialize(object_hook(obj), False)
             raise TypeError('can\'t serialize %r' % type(obj))
     return _serialize(data, False)
 
 
-def load(fp, charset='utf-8', errors='strict', decode_strings=False):
+def load(fp, charset='utf-8', errors='strict', decode_strings=False,
+         object_hook=None, array_hook=None):
     """Read a string from the open file object `fp` and interpret it as a
     data stream of PHP-serialized objects, reconstructing and returning
     the original object hierarchy.
@@ -156,7 +362,20 @@ def load(fp, charset='utf-8', errors='strict', decode_strings=False):
 
     `load` will read exactly one object from the stream.  See the docstring of
     the module for this chained behavior.
+
+    If an object hook is given object-opcodes are supported in the serilization
+    format.  The function is called with the class name and a dict of the
+    class data members.  The data member names are in PHP format which is
+    usually not what you want.  The `simple_object_hook` function can convert
+    them to Python identifier names.
+
+    If an `array_hook` is given that function is called with a list of pairs
+    for all array items.  This can for example be set to
+    `collections.OrderedDict` for an ordered, hashed dictionary.
     """
+    if array_hook is None:
+        array_hook = dict
+
     def _expect(e):
         v = fp.read(len(e))
         if v != e:
@@ -172,6 +391,21 @@ def load(fp, charset='utf-8', errors='strict', decode_strings=False):
                 raise ValueError('unexpected end of stream')
             buf.append(char)
         return ''.join(buf)
+
+    def _load_array():
+        items = int(_read_until(':')) * 2
+        _expect('{')
+        result = []
+        last_item = Ellipsis
+        for idx in xrange(items):
+            item = _unserialize()
+            if last_item is Ellipsis:
+                last_item = item
+            else:
+                result.append((last_item, item))
+                last_item = Ellipsis
+        _expect('}')
+        return result
 
     def _unserialize():
         type_ = fp.read(1).lower()
@@ -198,32 +432,32 @@ def load(fp, charset='utf-8', errors='strict', decode_strings=False):
             return data
         if type_ == 'a':
             _expect(':')
-            items = int(_read_until(':')) * 2
-            _expect('{')
-            result = {}
-            last_item = Ellipsis
-            for idx in xrange(items):
-                item = _unserialize()
-                if last_item is Ellipsis:
-                    last_item = item
-                else:
-                    result[last_item] = item
-                    last_item = Ellipsis
-            _expect('}')
-            return result
+            return array_hook(_load_array())
+        if type_ == 'o':
+            if object_hook is None:
+                raise ValueError('object in serialization dump but '
+                                 'object_hook not given.')
+            _expect(':')
+            name_length = int(_read_until(':'))
+            _expect('"')
+            name = fp.read(name_length)
+            _expect('":')
+            return object_hook(name, dict(_load_array()))
         raise ValueError('unexpected opcode')
 
     return _unserialize()
 
 
-def loads(data, charset='utf-8', errors='strict', decode_strings=False):
+def loads(data, charset='utf-8', errors='strict', decode_strings=False,
+          object_hook=None, array_hook=None):
     """Read a PHP-serialized object hierarchy from a string.  Characters in the
     string past the object's representation are ignored.
     """
-    return load(StringIO(data), charset, errors, decode_strings)
+    return load(StringIO(data), charset, errors, decode_strings,
+                object_hook, array_hook)
 
 
-def dump(data, fp, charset='utf-8', errors='strict'):
+def dump(data, fp, charset='utf-8', errors='strict', object_hook=None):
     """Write a PHP-serialized representation of obj to the open file object
     `fp`.  Unicode strings are encoded to `charset` with the error handling
     of `errors`.
@@ -231,12 +465,19 @@ def dump(data, fp, charset='utf-8', errors='strict'):
     `fp` must have a `write()` method that accepts a single string argument.
     It can thus be a file object opened for writing, a `StringIO` object, or
     any other custom object that meets this interface.
+
+    The `object_hook` is called for each unknown object and has to either
+    raise an exception if it's unable to convert the object or return a
+    value that is serializable (such as a `phpobject`).
     """
-    fp.write(dumps(data, charset, errors))
+    fp.write(dumps(data, charset, errors, object_hook))
 
 
 def dict_to_list(d):
     """Converts an ordered dict into a list."""
+    # make sure it's a dict, that way dict_to_list can be used as an
+    # array_hook.
+    d = dict(d)
     try:
         return [d[x] for x in xrange(len(d))]
     except KeyError:
@@ -253,5 +494,15 @@ unserialize = loads
 
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    def _runtests():
+        import doctest
+        import collections
+        # make the unittests pass on old pythons
+        if not hasattr(collections, 'OrderedDict'):
+            class _ODict(list):
+                def __repr__(self):
+                    return 'collections.OrderedDict(%s)' % list.__repr__(self)
+            collections.OrderedDict = _ODict
+        doctest.testmod()
+
+    _runtests()
--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def get_docs():
+    result = []
+    in_docs = False
+    f = open(os.path.join(os.path.dirname(__file__), 'phpserialize.py'))
+    try:
+        for line in f:
+            if in_docs:
+                if line.lstrip().startswith(':copyright:'):
+                    break
+                result.append(line[4:].rstrip())
+            elif line.strip() == 'r"""':
+                in_docs = True
+    finally:
+        f.close()
+    return '\n'.join(result)
 
 setup(
     name='phpserialize',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    version='1.1',
+    version='1.2',
     url='http://dev.pocoo.org/hg/phpserialize-main',
     py_modules=['phpserialize'],
     description='a port of the serialize and unserialize '
                 'functions of php to python.',
+    long_description=get_docs(),
+    zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: PHP',
